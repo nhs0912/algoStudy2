92a16f69997db23714fa43ca89e8be14572a847e
nhs0912 <nhs0912@nate.com>
2016-10-17 23:40:25 +0900

    괄호문제 완성
    알고스팟에서는 nonzero 문제 뜸...왜그런지 모르게음..

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by nhs09 on 2016-10-17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt= sc.nextInt();
+        for(int i=0; i<cnt;i++)
+        System.out.println("Hello Algospot!");
+    }
+}
