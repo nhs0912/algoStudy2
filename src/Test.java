01f0b100b2150351f0b2330182e2ceccb05eec3e
nhs0912 <nhs0912@nate.com>
2016-10-21 15:23:35 +0900

    LinkedList 구현

--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,36 @@
-import java.util.Scanner;
+import DataStructure.LinkedList;
+import DataStructure.ListNode;
 
 /**
  * Created by nhs09 on 2016-10-17.
  */
 public class Test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
-        int[] results = new int[cnt];
-        for (int i = 0; i < cnt; i++) {
-            int x = sc.nextInt(); //구하는 숫자 입력
-            if (x < 0) { //구하는 숫자가 음수이면
-                x = -x;
-            }
-            int n = 1;
-            int total;
-            while (true) {
-                total = n * (n + 1) / 2;//1부터 n까지의 총합
-                if (total >= x) {
-                    break;
-                }
-                n++;
-            }
-            while (true) {
-                if ((total - x) % 2 == 0) {//짝수이면 n의 값을 찾은 것
-                    results[i] = n;
-                    break;
-                    //System.out.println(n);
-                } else {
-                    n++;//그 다음 수
-                    total += n; //홀수 이면 그 다음 숫자를 더한다.
-                }
-            }
+        LinkedList L= new LinkedList();
+        System.out.println("1)공백 리스트에 노드 3개 삽입하기");
+        L.insertLastNode("월");
+        L.insertLastNode("화");
+        L.insertLastNode("수");
+        L.printList();
+
+        System.out.println("중간에 추가하기 ");
+        ListNode pre = L.searchNode("화");
+        if(pre ==null)
+        {
+            System.out.println("검색 실패 ");
         }
-        //결과 출력
-        for (int a : results) {
-            System.out.println(a);
+        else{
+            L.insertMiddleNode(pre,"목");
+            L.printList();
         }
+
+
+        System.out.println("역순으로 바꾸기 ");
+        L.reverseList();
+        L.printList();
+
+        System.out.println("마지막 노드 삭제하기");
+        L.deleteLastNode();
+        L.printList();
     }
 }
