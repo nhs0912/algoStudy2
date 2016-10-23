2dd053d43eed7b964c1864701a847ecc9f973dcc
nhs0912 <nhs0912@nate.com>
2016-10-23 11:34:22 +0900

    동전교환문제

--- src/Test.java
+++ src/Test.java
@@ -1,37 +1,11 @@
 
 
-import java.util.Queue;
-import java.util.Stack;
-
 /**
  * Created by nhs09 on 2016-10-21.
  */
 public class Test {
     public static void main(String[] args) {
-//        int[] numbers = new int[]{3, 1, 2, 7, 5, 11, 34, 8};
 //
-//
-//        for (int a : numbers)
-//            System.out.println(a);
-//
-//        Arrays.sort(numbers);
-//
-//        for (int a : numbers)
-//            System.out.println(a);
-
-
-        Stack stack = new Stack();
-
-        stack.add("1");
-        stack.add("3");
-        stack.add("2");
-
-
-        for(int i=0;i<stack.size();i++) {
-            System.out.println(stack.get(i));
-        }
-
-        Queue queue = new Queue;
 
     }
 }
