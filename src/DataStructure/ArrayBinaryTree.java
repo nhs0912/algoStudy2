181b6f5ef8206ecdec4296aa85af95c36412b745
nhs0912 <nhs0912@nate.com>
2016-10-30 13:11:26 +0900

    바이너리 트리

--- src/DataStructure/ArrayBinaryTree.java
+++ src/DataStructure/ArrayBinaryTree.java
@@ -3,5 +3,21 @@ package DataStructure;
 /**
  * Created by nhs09 on 2016-10-25.
  */
-public class ArrayBinaryTree {
+public class ArrayBinaryTree implements Tree{
+
+
+    @Override
+    public void insert(int[] arr, int num) {
+
+    }
+
+    @Override
+    public void delete(int[] arr, int num) {
+
+    }
+
+    @Override
+    public void lastDetele(int[] arr) {
+
+    }
 }
