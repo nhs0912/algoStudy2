c9bf89ea4b961d9078d3d7f1a3ff93a29f90f42f
nhs0912 <nhs0912@nate.com>
2016-11-04 12:57:08 +0900

    LinkedTree

---
+++ src/DataStructure/TreeNode.java
@@ -0,0 +1,10 @@
+package DataStructure;
+
+/**
+ * Created by heeseoknoh on 03/11/2016.
+ */
+public class TreeNode {
+    Object data;
+    TreeNode left;
+    TreeNode right;
+}
--- src/TreeMain.java
+++ src/TreeMain.java
@@ -1,5 +1,7 @@
 import DataStructure.Heap;
 import DataStructure.HeapSort;
+import DataStructure.LinkedTree;
+import DataStructure.TreeNode;
 
 /**
  * Created by nhs09 on 2016-10-31.
@@ -9,15 +11,19 @@ import DataStructure.HeapSort;
 public class TreeMain {
 
     public static void main(String[] args) {
-     //   Tree tree= new ArrayBinaryTree(8);
+        //Tree tree= new ArrayBinaryTree(8);
 
         Heap heap = new HeapSort(8);
-
-
-
-
-
-
+        LinkedTree T = new LinkedTree();
+        TreeNode n7 = T.makeBT(null, 'D', null);
+        TreeNode n6 = T.makeBT(null, 'C', null);
+        TreeNode n5 = T.makeBT(null, 'C', null);
+        TreeNode n4 = T.makeBT(null, 'A', null);
+        TreeNode n3 = T.makeBT(n6, '/', n7);
+        TreeNode n2 = T.makeBT(n4,'*',n5);
+        TreeNode n1 = T.makeBT(n2,'-',n3);
+        System.out.printf("\n Preorder : ");
+        T.preorder(n1);
 
 
     }
