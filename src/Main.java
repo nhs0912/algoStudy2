e8baca20145760bf3eebbeb5275f423e972978ca
nhs0912 <nhs0912@nate.com>
2016-11-11 13:16:28 +0900

    두번째로 큰 수

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,86 @@ import java.util.Scanner;
 /**
  * Created by nhs0912 on 2016-11-08.
  */
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
-        int sum=0;
-        for(int i=1;i<=n;i++)
-        {
-            sum+=i;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a == b) {
+            if (b < c) {
+                System.out.println(b);
+            } else if (b > c) {
+                System.out.println(c);
+            } else if (b == c) {
+                System.out.println(c);
+            }
+        } else if (a == c) {
+            if (c < b) {
+                System.out.println(c);
+            } else if (b < c) {
+                System.out.println(b);
+            } else if (b == a) {
+                System.out.println(b);
+            }
+        } else if (b == c) {
+            if (a < b) {
+                System.out.println(a);
+            } else if (b < a) {
+                System.out.println(b);
+            } else if (a == b) {
+                System.out.println(a);
+            }
+        } else if (a > b) {
+            if(b>c)
+            {
+                System.out.println(b);
+
+            }else if(b<c){
+                System.out.println(c);
+            }
         }
-        System.out.println(sum);
+
+
+//        if (a == b && b < c) {
+//            System.out.println(b);
+//        } else if (a == c && c < b) {
+//            System.out.println(c);
+//        } else if (b == c && c < a) {
+//            System.out.println(c);
+//        } else if (b == c && c > a) {
+//            System.out.println(a);
+//        } else if (a == b && b == c && c == a) {
+//            System.out.println(a);
+//        } else if (a == b && a < c) {
+//            System.out.println(a);
+//        } else if (a == b && a > c) {
+//            System.out.println(c);
+//        } else if (a == c && a < b) {
+//            System.out.println(a);
+//        } else if (a == c && a > b) {
+//            System.out.println(b);
+//        } else if (a < b && b < c) {
+//            System.out.println(b);
+//        } else if (a < b && b > c) {
+//            if (a > c) {
+//                System.out.println(a);
+//            } else if (c > a) {
+//                System.out.println(c);
+//            }
+//
+//        } else if (a > b && b < c) {
+//            if (a > c) {
+//                System.out.println(c);
+//            } else if (a < c) {
+//                System.out.println(a);
+//            }
+//
+//        } else if (a > b && b > c) {
+//            System.out.println(b);
+//        }
 
     }
 }
